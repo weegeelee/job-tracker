2ca734a3eaf23062e69f1c6b94de669974c37cbc
Yu Chen <weegeelee@hotmail.com>
2025-05-04 23:14:00 +0200

    change api

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
-
+console.log("API_BASE_URL:", API_BASE_URL);
 const api = axios.create({
     baseURL: API_BASE_URL,
     headers: {
