762cfcdb3188a475df68dade5b32fa976165eba4
Yu Chen <weegeelee@hotmail.com>
2025-05-04 23:00:44 +0200

    change listen port

--- server/server.ts
+++ server/server.ts
@@ -17,11 +17,9 @@ app.use("/", appRoutes);
 
 connectDB();
 
-if (process.env.NODE_ENV !== "production") {
-    const port = process.env.PORT || 5000;
-    app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
-    });
-}
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
 
 export default app;
